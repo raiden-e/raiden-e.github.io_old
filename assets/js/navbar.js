f20764303560292d0108aa6ef2989dccb17226b1
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-06-11 12:31:28 +0200

    add search

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,19 +2,19 @@
 
 var navbar = {
 
-  bigImgEl : null,
-  numImgs : null,
+  bigImgEl: null,
+  numImgs: null,
 
-  init : function() {
+  init: function () {
     setTimeout(navbar.initNavbar, 10);
 
     // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
+    $(window).scroll(function () {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar").addClass("top-nav-short");
+      } else {
+        $(".navbar").removeClass("top-nav-short");
+      }
     });
 
     // On mobile, hide the avatar when expanding the navbar menu
@@ -27,11 +27,12 @@ var navbar = {
 
     // show the big header image
     navbar.initImgs();
+    navbar.initSearch();
   },
 
-  initNavbar : function() {
+  initNavbar: function () {
     // Set the navbar-dark/light class based on its background color
-    const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
+    const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g, '').split(",");
     const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
       parseInt(rgb[0]) * 299 +
       parseInt(rgb[1]) * 587 +
@@ -44,7 +45,7 @@ var navbar = {
     }
   },
 
-  initImgs : function() {
+  initImgs: function () {
     // If the page was large images to randomly select from, choose an image
     if ($("#header-big-imgs").length > 0) {
       navbar.bigImgEl = $("#header-big-imgs");
@@ -58,7 +59,7 @@ var navbar = {
       navbar.setImg(src, desc);
 
       // For better UX, prefetch the next image so that it will already be loaded when we want to show it
-      var getNextImg = function() {
+      var getNextImg = function () {
         var imgInfo = navbar.getImgInfo();
         var src = imgInfo.src;
         var desc = imgInfo.desc;
@@ -67,14 +68,14 @@ var navbar = {
         prefetchImg.src = src;
         // if I want to do something once the image is ready: `prefetchImg.onload = function(){}`
 
-        setTimeout(function(){
+        setTimeout(function () {
           var img = $("<div></div>").addClass("big-img-transition").css("background-image", 'url(' + src + ')');
           $(".intro-header.big-img").prepend(img);
-          setTimeout(function(){ img.css("opacity", "1"); }, 50);
+          setTimeout(function () { img.css("opacity", "1"); }, 50);
 
           // after the animation of fading in the new image is done, prefetch the next one
           //img.one("transitioned webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-          setTimeout(function() {
+          setTimeout(function () {
             navbar.setImg(src, desc);
             img.remove();
             getNextImg();
@@ -90,24 +91,43 @@ var navbar = {
     }
   },
 
-  getImgInfo : function() {
+  getImgInfo: function () {
     var randNum = Math.floor((Math.random() * navbar.numImgs) + 1);
     var src = navbar.bigImgEl.attr("data-img-src-" + randNum);
     var desc = navbar.bigImgEl.attr("data-img-desc-" + randNum);
 
     return {
-      src : src,
-      desc : desc
+      src: src,
+      desc: desc
     }
   },
 
-  setImg : function(src, desc) {
+  setImg: function (src, desc) {
     $(".intro-header.big-img").css("background-image", 'url(' + src + ')');
     if (typeof desc !== typeof undefined && desc !== false) {
       $(".img-desc").text(desc).show();
     } else {
       $(".img-desc").hide();
     }
+  },
+
+  initSearch: function () {
+    if (!document.getElementById("beautifuljekyll-search-overlay")) {
+      return;
+    }
+
+    $("#nav-search-link").click(function (e) {
+      e.preventDefault();
+      $("#beautifuljekyll-search-overlay").show();
+      $("#nav-search-input").focus().select();
+    });
+    $("#nav-search-exit").click(function (e) {
+      e.preventDefault();
+      $("#beautifuljekyll-search-overlay").hide();
+    });
+    $(document).on('keyup', function (e) {
+      if (e.key == "Escape") $("#beautifuljekyll-search-overlay").hide();
+    });
   }
 };
 
--- copy-this.md
+++ copy-this.md
@@ -2,12 +2,13 @@
 layout: default
 title: Music - Raiden Erdmann
 subtitle: I love electronic music and mix it myself.
+permalink: /copy-this
 ---
 # Copy this
-<div id="content"></div>
+<div id='content'></div>
 <p>Into your C:>onsole</p>
 <script>
   async function onLoad() {
-    document.getElementById("content").innerHTML = new URLSearchParams(location.search).get("id");
+    $("#content").append(new URLSearchParams(location.search).get("id"));
   }
 </script>
